4f94961e9e6960501a0ae8db55892b23b82d58d1
Vinícius Amaral <amaralvinicius171@gmail.com>
2023-05-13 23:58:42 -0300

    Adicionado score dos jogadores na tela
    E algumas correções da movimentação do player

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ pygame.display.set_caption('PyPong')
 
 run = True
 
+def text(font, color,text):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    return (text_surface, text_rect)
+
 def ball_moviment(ball_rect, speed):
     ball_rect.x += speed[0]
     ball_rect.y += speed[1]
@@ -23,23 +28,28 @@ def ball_moviment(ball_rect, speed):
         ball_rect.bottom = screen_height
         speed[1] *= -1
     if ball_rect.right >= screen_width:
+        game_score['opponent_score'] += 1
         ball_rect.right = screen_width
         speed[0] *= -1
         ball_rect = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 20, 20)
     if ball_rect.left <= 0:
+        game_score['player_score'] += 1
         ball_rect.left = 0
         speed[0] *= -1
         ball_rect = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 20, 20)
 
     return ball_rect
 
+font = pygame.font.SysFont("arialblack", 30)
+game_score = {'opponent_score': 0, 'player_score': 0}
+
 player = pygame.Rect(screen_width - 30, screen_height / 2 - 70, 20, 140)
 player_direction = 'idle'
 down_button = False
-up_button = True
+up_button = False
 player_speed  = 0
 opponent = pygame.Rect(10, screen_height / 2 - 70, 20, 140)
-opponent_speed = 7
+opponent_speed = 5
 ball = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 20, 20)
 ball_speed = [7, 7]
 
@@ -51,22 +61,22 @@ while run:
         if event.type == QUIT:
             run = False
         if event.type == KEYDOWN:
-            if event.key == K_DOWN and player.bottom < screen_height:
+            if event.key == K_DOWN and player.bottom <= screen_height:
                 player_direction = 'down'
                 down_button = True
-            if event.key == K_UP and player.top > 0:
+            if event.key == K_UP and player.top >= 0:
                 player_direction = 'up'
                 up_button = True
         if event.type == KEYUP:
             if event.key == K_DOWN:
                 down_button = False
-                if up_button and player.top > 0:
+                if up_button and player.top >= 0:
                     player_direction = 'up'
                 else:
                     player_direction = 'idle'
             if event.key == K_UP:
                 up_button = False
-                if down_button and player.bottom < screen_height:
+                if down_button and player.bottom <= screen_height:
                     player_direction = 'down'
                 else:
                     player_direction = 'idle'
@@ -104,6 +114,17 @@ while run:
     pygame.draw.rect(screen, (225, 225, 225), player)
     pygame.draw.rect(screen, (225, 225, 225), opponent)
     pygame.draw.rect(screen, (225, 225, 225), ball)
+
+    opponent_score, opponent_score_rect = text(font, (255, 255, 255), f'{game_score["opponent_score"]}')
+    opponent_score_rect.y = screen_height / 2
+    opponent_score_rect.right = screen_width / 2 - 20
+
+    player_score, player_score_rect = text(font, (255, 255, 255), f'{game_score["player_score"]}')
+    player_score_rect.y = screen_height / 2
+    player_score_rect.left = screen_width / 2 + 20
+
+    screen.blit(opponent_score, opponent_score_rect)
+    screen.blit(player_score, player_score_rect)
 
     clock.tick(60)
     pygame.display.update()
