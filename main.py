a9427e89eac372821138b63b2767fb11fb1e39ab
Vinícius Amaral <amaralvinicius171@gmail.com>
2023-05-13 23:00:15 -0300

    Melhorei a lógica do player

--- main.py
+++ main.py
@@ -25,14 +25,21 @@ def ball_moviment(ball_rect, speed):
     if ball_rect.right >= screen_width:
         ball_rect.right = screen_width
         speed[0] *= -1
+        ball_rect = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 20, 20)
     if ball_rect.left <= 0:
         ball_rect.left = 0
         speed[0] *= -1
+        ball_rect = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 20, 20)
+
+    return ball_rect
 
 player = pygame.Rect(screen_width - 30, screen_height / 2 - 70, 20, 140)
+player_direction = 'idle'
+down_button = False
+up_button = True
 player_speed  = 0
 opponent = pygame.Rect(10, screen_height / 2 - 70, 20, 140)
-opponent_speed = 10
+opponent_speed = 7
 ball = pygame.Rect(screen_width / 2 - 10, screen_height / 2 - 10, 20, 20)
 ball_speed = [7, 7]
 
@@ -45,14 +52,24 @@ while run:
             run = False
         if event.type == KEYDOWN:
             if event.key == K_DOWN and player.bottom < screen_height:
-                player_speed += 10
+                player_direction = 'down'
+                down_button = True
             if event.key == K_UP and player.top > 0:
-                player_speed += -10
+                player_direction = 'up'
+                up_button = True
         if event.type == KEYUP:
             if event.key == K_DOWN:
-                player_speed -= 10
+                down_button = False
+                if up_button and player.top > 0:
+                    player_direction = 'up'
+                else:
+                    player_direction = 'idle'
             if event.key == K_UP:
-                player_speed -= -10
+                up_button = False
+                if down_button and player.bottom < screen_height:
+                    player_direction = 'down'
+                else:
+                    player_direction = 'idle'
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed[0] *= -1
@@ -61,6 +78,15 @@ while run:
         opponent.y  += opponent_speed
     if opponent.center[1] > ball.center[1] and opponent.top > 0:
         opponent.y -= opponent_speed
+
+    if player_direction == 'idle':
+        player_speed = 0
+    elif player_direction == 'down':
+        player_speed = 10
+    elif player_direction == 'up':
+        player_speed = -10
+
+    player.y += player_speed
 
     if player.top <= 0:
         player.top = 0
@@ -72,8 +98,7 @@ while run:
     if opponent.bottom >= screen_height:
         opponent.bottom = screen_height
 
-    ball_moviment(ball, ball_speed)
-    player.y += player_speed
+    ball = ball_moviment(ball, ball_speed)
 
     pygame.draw.line(screen, (200, 200, 200), (screen_width / 2 - 1, 0), (screen_width / 2 - 1, screen_height), width = 2)
     pygame.draw.rect(screen, (225, 225, 225), player)
